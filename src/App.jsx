89b4072a7de61a6638cf3bd5b5afe3f98c9cc0da
jycurien <curien.jeanyves@gmail.com>
2023-03-16 23:42:09 +0100

    add login form

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Main, { mainLoader } from './layouts/Main'
 import logoutAction from './actions/logout'
 
 // Routes
-import Dashboard, { dashboardLoader } from './pages/Dashboard'
+import Dashboard, { dashboardAction, dashboardLoader } from './pages/Dashboard'
 import Error from './pages/Error'
 import { SnackbarProvider } from 'notistack'
 
@@ -21,6 +21,7 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <Dashboard />,
+        action: dashboardAction,
         loader: dashboardLoader,
       },
       {
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,9 @@
 import { useLoaderData } from 'react-router-dom'
+import { enqueueSnackbar } from 'notistack'
 import { fetchData } from '../helpers'
+
+// components
+import Intro from '../components/Intro'
 
 // loader
 export const dashboardLoader = async () => {
@@ -7,15 +11,24 @@ export const dashboardLoader = async () => {
   return { userName }
 }
 
+// action
+export const dashboardAction = async ({ request }) => {
+  const data = await request.formData()
+  const formData = Object.fromEntries(data)
+  try {
+    localStorage.setItem('userName', JSON.stringify(formData.userName))
+    return enqueueSnackbar(`Welcome ${formData.userName}`, {
+      variant: 'success',
+    })
+  } catch (error) {
+    throw new Error('There was a problem creating you account.')
+  }
+}
+
 const Dashboard = () => {
   const { userName } = useLoaderData('userName')
 
-  return (
-    <div>
-      <h1>{userName}</h1>
-      Dashboard
-    </div>
-  )
+  return <>{userName ? <p>{userName}</p> : <Intro />}</>
 }
 
 export default Dashboard
